9abe24fc0b9a613c8034e4166932d045cb3fa77f
Jochimin <54875738+jochimin88@users.noreply.github.com>
2020-11-18 10:47:05 -0400

    Update mssql-server-docker.sh

--- mssql-server-docker.sh
+++ mssql-server-docker.sh
@@ -1,5 +1,3 @@
 
-# Up MS SQL SERVER DATABASE on port 1433 and password Jochimin007
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Jochimin007' -p 1433:1433 -d microsoft/mssql-server-linux:2019-lastest
-
-
+# Up MS SQL SERVER DATABASE on port 1433 and password YouPassWord
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=YouPassWord' -e "MSSQL_PID=Express" -p 1433:1433 -d microsoft/mssql-server-linux:2019-lastest
